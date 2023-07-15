a781ddc0ff57bde1a4a189ae3b8acc50a09b4dda
IAbrahanarley <abrahanarley96@gmail.com>
2023-07-14 22:02:53 -0300

    FindByDeparmentId e ajustes

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,17 +1,31 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-	
-		SellerDao sellerDao = DaoFactory.createSellerDao();
-		Seller seller = sellerDao.findById(3);
 		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		System.out.println("========= Test1: Seller FindById =========");
+		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
+
+		System.out.println("========= Test2: Seller FindByDepartmentId =========");
+		Department department = new Department(2, null);
+		List<Seller> list = sellerDao.findByDeparmentId(department);
+		for (Seller obj : list) {
+			System.out.println(obj);
+			
+		}
+		
 	}
 }
